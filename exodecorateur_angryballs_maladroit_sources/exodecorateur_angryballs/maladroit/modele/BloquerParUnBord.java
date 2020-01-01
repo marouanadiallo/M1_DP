8aeed6de204d960ccf4b8fca239a0c6f089e0045
wanemar224 <alpha-marouana.diallo9@etu.univ-lorraine.fr>
2020-01-01 17:41:14 +0100

    version finale

--- exodecorateur_angryballs_maladroit_sources/exodecorateur_angryballs/maladroit/modele/BloquerParUnBord.java
+++ exodecorateur_angryballs_maladroit_sources/exodecorateur_angryballs/maladroit/modele/BloquerParUnBord.java
@@ -1,6 +1,5 @@
 package exodecorateur_angryballs.maladroit.modele;
 
-import java.util.Vector;
 
 import mesmaths.cinematique.Collisions;
 
@@ -9,11 +8,6 @@ public class BloquerParUnBord extends DecorateurBille{
 	public BloquerParUnBord(Bille bille) {
 		super(bille);
 		// TODO Auto-generated constructor stub
-	}
-	@Override
-	public void gestionAcceleration(Vector<Bille> billes)
-	{
-		this.billeAdecorer.gestionAcceleration(billes);  
 	}
 
 	@Override
